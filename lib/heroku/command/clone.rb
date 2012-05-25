d20e2ee75b480427951848d95a3f4c22aa198306
Jake Moffatt <jakeonrails@gmail.com>
2012-05-24 21:53:56 -0700

    Clone correctly parses command line now

--- lib/heroku/command/clone.rb
+++ lib/heroku/command/clone.rb
@@ -4,34 +4,36 @@ require "heroku/command/base"
 #
 class Heroku::Command::Clone < Heroku::Command::Base
 
-  # clone PATH
+  # clone NEWAPP
   #
   # create a new app instance on heroku, that is built the same as an existing
   # app
   #
-  # -d, --dest DEST # output info as raw key/value pairs
-  #
   def index
+    new_app = shift_argument
+
     validate_arguments!
 
-    dest = options[:dest]
-    path = shift_argument
-
-    #dest = extract_option('--dest')
-    #path = extract_option('--path')
 
     # create a new app
     app_data = api.get_app(app).body
     hputs app_data.inspect
 
     git_repo = app_data['git_url']
-    hputs git_repo
+    hputs("git clone #{git_repo} #{new_app}")
 
-    hputs("git clone #{git_repo} #{path}/#{dest}")
+    hputs("cd #{new_app}")
 
-    # clone the addons (databases, 3rd party services)
-    addons_data = api.get_addons(app).body.map {|addon| addon["description"]}
-    hputs addons_data.inspect
+    # use the source app's stack
+    stack = app_data["stack"]
+
+    # use the source app's addons
+    addons = api.get_addons(app).body.map {|addon| addon["name"]}.join(",")
+
+    hputs("heroku create #{new_app} --stack #{stack} --addons #{addons}")
+
+    # remove the origin that git created during cloning
+    hputs("git remote rm origin")
 
     # clone collaborators
     collaborators_data = api.get_collaborators(app).body.map {|collaborator| collaborator["email"]}
